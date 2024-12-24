d9a64fe306510eead225e7bed7022c31a60efc55
ErikYandex <erikstankevich@yandex.com>
2024-12-24 22:46:13 +0000

    Move if statement in while loop, to see if while loop works

--- sandbox/RedBlock.lua
+++ sandbox/RedBlock.lua
@@ -5,15 +5,17 @@ RedBrick.Parent = game.Workspace
 RedBrick.BrickColor = BrickColor.new("Bright red")
 RedBrick.Size = Vector3.new(2,2,2)
 
-if RedBrick.BrickColor == BrickColor.new("Bright red") then
-	wait(3)
-	RedBrick.BrickColor = BrickColor.new("Bright blue")
-else
-	print("bar")
-end
 
 
 while True do
 	wait(0.1)
 	RedBrick.Size = RedBrick.Size + Vector3.new(increment, increment, increment)
+
+	if RedBrick.BrickColor == BrickColor.new("Bright red") then
+		wait(3)
+		RedBrick.BrickColor = BrickColor.new("Bright blue")
+	else
+		print("bar")
+	end
+
 end
