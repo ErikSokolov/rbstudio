25a4cebbda8eeee4472a65c9e78ac7dc85285aed
ErikYandex <erikstankevich@yandex.com>
2024-12-24 22:36:17 +0000

    Change size of cube to 0 and increment every 1/10 second

--- sandbox/RedBlock.lua
+++ sandbox/RedBlock.lua
@@ -3,11 +3,17 @@ RedBrick = Instance.new("Part")
 RedBrick.Parent = game.Workspace
 
 RedBrick.BrickColor = BrickColor.new("Bright red")
-RedBrick.Size = Vector3.new(5,5,5)
+RedBrick.Size = Vector3.new(0,0,0)
 
 if RedBrick.BrickColor == BrickColor.new("Bright red") then
 	wait(3)
 	RedBrick.BrickColor = BrickColor.new("Bright blue")
 else
 	print("bar")
+end
+
+
+while True do
+	wait(0.1)
+	RedBrick.Size =RedBrick.Size + Vector3.new(increment, increment, increment)
 end
