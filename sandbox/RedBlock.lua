a065fb4e83311677fc0516024c059efbe57ebb7b
ErikYandex <erikstankevich@yandex.com>
2024-12-24 22:17:25 +0000

    Set size of RedBrick, should be cube

--- sandbox/RedBlock.lua
+++ sandbox/RedBlock.lua
@@ -3,6 +3,7 @@ RedBrick = Instance.new("Part")
 RedBrick.Parent = game.Workspace
 
 RedBrick.BrickColor = BrickColor.new("Bright red")
+RedBrick.Size = Vector3.new(5,5,5)
 
 if RedBrick.BrickColor == BrickColor.new("Bright red") then
 	wait(3)
