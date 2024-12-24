ab85622f36714470d7adf6cfb842f166e72dac34
ErikYandex <erikstankevich@yandex.com>
2024-12-24 21:00:10 +0000

    Do GreenBrick again on laptop to see how i will open script in rbstudio

---
+++ GreenBrick.lua
@@ -0,0 +1,10 @@
+GreenBrick = Instance.new("Part")
+GreenBrick.Parent = game.Workspace
+
+GreenBrick.Brickcolor.new("Bright green")
+
+if GreenBrick.BrickColor == BrickColor.new("Bright green") then
+	print("foo")
+else
+	print("bar")
+end
