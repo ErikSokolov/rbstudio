fe6bd1062ee1f93cdb93026ebb4e771db90fd06c
ErikYandex <erikstankevich@yandex.com>
2024-12-25 07:17:57 +0000

    #

--- sandbox/Floor.lua
+++ sandbox/Floor.lua
@@ -1,9 +1,9 @@
 BlueWall = Instance.new("Part")
 BlueWall.Parent = game.Workspace
 
-BlueWall.BrickColor = BrickColor.new("black")
+BlueWall.BrickColor = BrickColor.new("Black")
 BlueWall.Size = Vector3.new(10099,9,10000)
-BlueWall.Position = Vector3.new(2,-20,2)
+BlueWall.Position = Vector3.new(0,4,0)
 BlueWall.Anchored = true
 BlueWall.CastShadow = false
 
