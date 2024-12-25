5c220f953714802ac06cb50af3ba1b68662a0c78
ErikYandex <erikstankevich@yandex.com>
2024-12-25 07:23:23 +0000

    #

--- sandbox/BlueWall.lua
+++ sandbox/BlueWall.lua
@@ -1,7 +1,7 @@
 BlueWall = Instance.new("Part")
 BlueWall.Parent = game.Workspace
 
-BlueWall.BrickColor = BrickColor.new("Bright blue")
+BlueWall.BrickColor = BrickColor.new("White")
 BlueWall.Size = Vector3.new(10099,5009,0.2)
 BlueWall.Position = Vector3.new(2,0,2)
 BlueWall.Anchored = true
--- sandbox/GreenWall.lua
+++ sandbox/GreenWall.lua
@@ -1,7 +1,7 @@
 GreenWall = Instance.new("Part")
 GreenWall.Parent = game.Workspace
 
-GreenWall.BrickColor = BrickColor.new("Bright green")
+GreenWall.BrickColor = BrickColor.new("White")
 GreenWall.Size = Vector3.new(10099,5009,0.2)
 GreenWall.Position = Vector3.new(-2,0,-2)
 GreenWall.Anchored = true
