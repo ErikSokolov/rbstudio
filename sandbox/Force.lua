568623ceceba1ff33776d20beb3b32d01c3a6b19
ErikYandex <erikstankevich@yandex.com>
2024-12-25 06:41:43 +0000

    #

--- sandbox/Force.lua
+++ sandbox/Force.lua
@@ -3,12 +3,12 @@ vorce.Parent = game.Workspace
 
 vorce.BrickColor = BrickColor.new("Bright red")
 vorce.Size = Vector3.new(1000, 5000, 50)
-vorce.Position = Vector3.new(5,0,5)
+vorce.Position = Vector3.new(5,0,50)
 vorce.Anchored = true
 vorce.CFrame = vorce.CFrame * CFrame.Angles(0, math.rad(90),0)
 vorce.CastShadow = false
 
 while true do
-	task.wait(0.1)
-	vorce.Position = vorce.Position + Vector3.new(-2,0,0)
+	task.wait(0.01)
+	vorce.Position = vorce.Position + Vector3.new(-0.5,0,0)
 end
