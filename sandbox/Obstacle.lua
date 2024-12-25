e02831a9d3f08d93eb49b68f1df3207828e34b62
ErikYandex <erikstankevich@yandex.com>
2024-12-25 07:23:34 +0000

    #

---
+++ sandbox/Obstacle.lua
@@ -0,0 +1,8 @@
+Obst= Instance.new("Part")
+Obst.Parent = game.Workspace
+
+Obst.BrickColor = BrickColor.new("Pink")
+Obst.Size = Vector3.new(1,1,1)
+Obst.Position = Vector3.new(0,8,0)
+Obst.Anchored = true
+
