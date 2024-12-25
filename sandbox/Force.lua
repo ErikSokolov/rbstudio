271483548826ee4f92c723aa388004f9d204d970
ErikYandex <erikstankevich@yandex.com>
2024-12-25 06:20:48 +0000

    #

---
+++ sandbox/Force.lua
@@ -0,0 +1,13 @@
+Force = Instance.new("Part")
+Force.Parent = game.Workspace
+
+Force.BrickColor = Brickcolor.new("Bright red")
+Force.size = (1000, 5000, 3)
+Force.Position = Vector3.new(5,0,5)
+Force.Anchored = true
+Force.CFrame = Force.CFrame * CFrame.Angles(0, math.rad(90),0)
+
+while true do
+	task.wait(1)
+	Force.Position = Force.Position + Vector3.new(1,0,0)
+end
