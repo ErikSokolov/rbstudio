bf74da001a1236f695997fda176b612254ee552e
ErikYandex <erikstankevich@yandex.com>
2024-12-25 07:50:11 +0000

    #

--- sandbox/Obstacle.lua
+++ sandbox/Obstacle.lua
@@ -3,11 +3,11 @@ Obst.Parent = game.Workspace
 
 Obst.BrickColor = BrickColor.new("Pink")
 Obst.Size = Vector3.new(1,2,20)
-Obst.Position = Vector3.new(-10,11,0)
+Obst.Position = Vector3.new(30,11,0)
 Obst.Anchored = true
 
 while true do
 	task.wait(10)
-	Obst.Position = Obst.Position * Vector3.new(2,1,1)
+	Obst.Position = Obst.Position * Vector3.new(7,1,1)
 end
 
