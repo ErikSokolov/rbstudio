15bd8dcfe93e8f4a0d19596d180addd2aaee0568
ErikYandex <erikstankevich@yandex.com>
2024-12-25 07:32:07 +0000

    #

--- sandbox/Obstacle.lua
+++ sandbox/Obstacle.lua
@@ -6,7 +6,7 @@ Obst.Size = Vector3.new(1,1,1)
 Obst.Position = Vector3.new(-10,20,0)
 Obst.Anchored = true
 
-while True
+while True do
 	task.wait(3)
 	Obst.Position = Obst.Position + Vector3.new(-20,0,0)
 end
