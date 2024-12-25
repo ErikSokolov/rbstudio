dc19c65c1819170688379bf16d4749f871063f22
ErikYandex <erikstankevich@yandex.com>
2024-12-25 04:18:20 +0000

    Change cube position
    Cube is unanchored but does not fly

--- sandbox/RedBlock.lua
+++ sandbox/RedBlock.lua
@@ -4,7 +4,7 @@ RedBrick.Parent = game.Workspace
 
 RedBrick.BrickColor = BrickColor.new("Bright red")
 RedBrick.Size = Vector3.new(2,2,2)
-RedBrick.Position = Vector3.new(10,0,0)
+RedBrick.Position = Vector3.new(0,10,0)
 RedBrick.Ankored = false
 
 
