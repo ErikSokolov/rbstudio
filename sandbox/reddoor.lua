4b1d5d41c765099998c8e50880b26f67d8af27e5
ErikYandex <erikstankevich@yandex.com>
2024-12-26 03:28:40 +0000

    #

---
+++ sandbox/reddoor.lua
@@ -0,0 +1,24 @@
+redwall= script.Parent
+
+
+redwall.BrickColor = BrickColor.new("Dark red")
+i=0
+maxx = 10
+waiter = 1
+
+
+while i < maxx do 
+	redwall.Position = redwall.Position + Vector3.new(2,0,0)
+	task.wait(waiter)
+	i = i + 1
+end
+
+if i == maxx then
+	while i > 0 do
+		redwall.Position = redwall.Position + Vector3.new(-2,0,0)
+		task.wait(waiter)
+		i = i - 1
+	end
+end
+
+
