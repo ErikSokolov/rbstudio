3c6738499ec32acc928181fba3435b382484cd65
ErikYandex <erikstankevich@yandex.com>
2024-12-26 03:33:06 +0000

    #

--- sandbox/greendoor.lua
+++ sandbox/greendoor.lua
@@ -4,21 +4,22 @@ greenwall = script.Parent
 greenwall.BrickColor = BrickColor.new("Dark green")
 i=0
 maxx = 10
-waiter = 1
+waiter = 0.3
 
 
-while i < maxx do 
-	greenwall.Position = greenwall.Position + Vector3.new(2,0,0)
-	task.wait(waiter)
-	i = i + 1
-end
-
-if i == maxx then
-	while i > 0 do
-		greenwall.Position = greenwall.Position + Vector3.new(-2,0,0)
+while True do
+	while i < maxx do 
+		greenwall.Position = greenwall.Position + Vector3.new(2,0,0)
 		task.wait(waiter)
-		i = i - 1
+		i = i + 1
+	end
+
+	if i == maxx then
+		while i > 0 do
+			greenwall.Position = greenwall.Position + Vector3.new(-2,0,0)
+			task.wait(waiter)
+			i = i - 1
+		end
 	end
 end
-
 
--- sandbox/reddoor.lua
+++ sandbox/reddoor.lua
@@ -4,21 +4,22 @@ redwall= script.Parent
 redwall.BrickColor = BrickColor.new("Dark red")
 i=0
 maxx = 10
-waiter = 1
+waiter = 0.3
 
 
-while i < maxx do 
-	redwall.Position = redwall.Position + Vector3.new(-2,0,0)
-	task.wait(waiter)
-	i = i + 1
-end
-
-if i == maxx then
-	while i > 0 do
-		redwall.Position = redwall.Position + Vector3.new(2,0,0)
+while True do
+	while i < maxx do 
+		redwall.Position = redwall.Position + Vector3.new(-2,0,0)
 		task.wait(waiter)
-		i = i - 1
+		i = i + 1
+	end
+
+	if i == maxx then
+		while i > 0 do
+			redwall.Position = redwall.Position + Vector3.new(2,0,0)
+			task.wait(waiter)
+			i = i - 1
+		end
 	end
 end
-
 
