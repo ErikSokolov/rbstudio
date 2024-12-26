e6d145cae7a7ba3f40b7430602c580e2100dd1f8
ErikYandex <erikstankevich@yandex.com>
2024-12-26 03:25:42 +0000

    #

--- sandbox/greendoor.lua
+++ sandbox/greendoor.lua
@@ -11,12 +11,11 @@ while i < maxx do
 	greenwall.Position = greenwall.Position + Vector3.new(2,0,0)
 	task.wait(waiter)
 	i = i + 1
-	return greenwall.Position
 end
 
 if i == maxx then
 	while i > 0 do
-		greenwall.Position = greenwall.Positon + Vector3.new(-2,0,0)
+		greenwall.Position = greenwall.Position + Vector3.new(-2,0,0)
 		task.wait(waiter)
 		i = i - 1
 	end
