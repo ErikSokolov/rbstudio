22059d27fff03c10c22bcc7a652c5de7f69a8974
ErikYandex <erikstankevich@yandex.com>
2024-12-26 03:14:39 +0000

    #

--- sandbox/greendoor.lua
+++ sandbox/greendoor.lua
@@ -1,8 +1,12 @@
 greenwall = script.Parent
 
-greenwall.BrickColor = BrickColor.new("Dark green")
 
-while true do 
+greenwall.BrickColor = BrickColor.new("Dark green")
+i=0
+
+
+while i < 10 do 
 	greenwall.Position = greenwall.Position + Vector3.new(2,0,0)
 	task.wait(0.1)
+	i = i + 1
 end
